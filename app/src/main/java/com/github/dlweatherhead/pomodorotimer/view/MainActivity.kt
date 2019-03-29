20db95f924f7e412344e294d1209013940196253
David Weatherhead <dlweatherhead@gmail.com>
2019-03-29 15:50:47 +0100

    Implemented a menu action for accessing settings menu (#1)
    - Settings screen is not implemented. A TODO was added for where navigation to the settings screen can be done

--- app/src/main/java/com/github/dlweatherhead/pomodorotimer/view/MainActivity.kt
+++ app/src/main/java/com/github/dlweatherhead/pomodorotimer/view/MainActivity.kt
@@ -4,6 +4,9 @@ import android.databinding.DataBindingUtil
 import android.databinding.Observable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import com.github.dlweatherhead.pomodorotimer.R
 import com.github.dlweatherhead.pomodorotimer.databinding.ActivityMainBinding
@@ -25,11 +28,24 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
         binding.viewModel = viewModel
         binding.timeFormatter = TimeFormatUtility()
         binding.timerButton.setOnClickListener { viewModel.handleTimerButtonClicked() }
         viewModel.showToastTrigger.addOnPropertyChangedCallback(showToastOnPropertyChangedCallback)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.action_preferences) {
+            // TODO: Navigate to settings screen
+            Log.e("MainActivity", "Action Preferences clicked!")
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
